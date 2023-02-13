d8260e73eb1dc420af37b4bd78d7cafe74a6b819
MohamedAbdelrazek22 <118555438+MohamedAbdelRazek222@users.noreply.github.com>
2023-02-13 07:09:17 +0200

    comment

---
+++ src/context.js
@@ -0,0 +1,82 @@
+import React, { useContext, useEffect, useReducer } from 'react'
+
+import {
+  SET_LOADING,
+  SET_STORIES,
+  REMOVE_STORY,
+  HANDLE_PAGE,
+  HANDLE_SEARCH,
+} from './actions'
+import reducer from './reducer'
+
+const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?'
+
+const initialState = {
+  isLoading:true,
+  hits:[],
+  query:'react',//search دي بتاعه ال 
+  page:0,
+  nbPages:0
+}
+
+const AppContext = React.createContext()
+
+const AppProvider = ({ children }) => {
+
+const [state,dispatch]=useReducer(reducer,initialState)
+
+// fetchStories--------------------------------------------
+const fetchStories=async (url) =>{
+
+dispatch({type:SET_LOADING}) //دي بتاعه ال loading
+
+try{
+
+  const response=await  fetch(url)
+  const data=await response.json()
+  dispatch({type:SET_STORIES,payload:{hits:data.hits,nbPages:data.nbPages}})
+  console.log({data})
+}catch(error){
+
+  console.log(error)
+}
+
+}
+// removeStory----------------------------------------
+const removeStory=(id)=>{
+console.log(id) 
+dispatch({type:REMOVE_STORY,payload:id})
+}
+// handleSearch----------------------------------------
+const handleSearch=(query)=>{
+  console.log(query)
+  dispatch({type:HANDLE_SEARCH,payload:query})
+
+}
+// handlePage--------------------------------------------
+const handlePage=(value)=>{
+  console.log(value)
+  dispatch({type:HANDLE_PAGE,payload:value})
+}
+
+// useEffect--------------------------------------------
+useEffect(()=>{
+fetchStories(`${API_ENDPOINT}query=${state.query}&page=${state.page}`)
+
+},[state.query,state.page])
+
+  return <AppContext.Provider value={{ 
+    
+...state,
+removeStory,
+handleSearch,
+handlePage
+
+   }}>{children}</AppContext.Provider>
+}
+// make sure use
+export const useGlobalContext = () => {
+  return useContext(AppContext)
+}
+
+export { AppContext, AppProvider }
